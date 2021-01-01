e3ad9b8959310c8a7b6d5f0b018dcc570cd7d558
WooParadog <guohaochuan@gmail.com>
2021-01-01 23:50:10 +0800

    f

--- rc.lua
+++ rc.lua
@@ -46,7 +46,8 @@ local autostarts = {
   "picom",
   "urxvtd -q -f -o",
   "unclutter -root",
-  "light-locker",
+  --"ibus-daemon -drx",
+  --"light-locker",
   --"xbindkeys" 
 }
 
@@ -285,16 +286,17 @@ globalkeys = awful.util.table.join(
 )
 
 clientkeys = awful.util.table.join(
-   awful.key({ modkey, "Shift" }, "c", function (c) c:kill() end, {description = "close", group = "client"}),
+  awful.key({ modkey,           }, "f",      function (c) c.fullscreen = not c.fullscreen  end),
+  awful.key({ modkey, "Shift" }, "c", function (c) c:kill() end, {description = "close", group = "client"}),
 
-   awful.key({ modkey, "Control" }, "space", awful.client.floating.toggle , {description = "toggle floating", group = "client"}),
-   awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end, {description = "move to master", group = "client"}),
+  awful.key({ modkey, "Control" }, "space", awful.client.floating.toggle , {description = "toggle floating", group = "client"}),
+  awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end, {description = "move to master", group = "client"}),
 
-   awful.key({ modkey, }, "o", function (c) c:move_to_screen() end, {description = "move to screen", group = "client"}),
-   awful.key({ modkey, }, "t", function (c) c.ontop = not c.ontop end, {description = "toggle keep on top", group = "client"}),
-   awful.key({ modkey, }, "y", function (c) c.sticky = not c.sticky end, {description = "toggle sticky on top", group = "client"}),
-   awful.key({ modkey, }, "n", function (c) c.minimized = true end , {description = "minimize", group = "client"}),
-   awful.key({ modkey, }, "m", function (c) c.maximized = not c.maximized c:raise() end , {description = "maximize", group = "client"})
+  awful.key({ modkey, }, "o", function (c) c:move_to_screen() end, {description = "move to screen", group = "client"}),
+  awful.key({ modkey, }, "t", function (c) c.ontop = not c.ontop end, {description = "toggle keep on top", group = "client"}),
+  awful.key({ modkey, }, "y", function (c) c.sticky = not c.sticky end, {description = "toggle sticky on top", group = "client"}),
+  awful.key({ modkey, }, "n", function (c) c.minimized = true end , {description = "minimize", group = "client"}),
+  awful.key({ modkey, }, "m", function (c) c.maximized = not c.maximized c:raise() end , {description = "maximize", group = "client"})
 )
 
 -- Bind all key numbers to tags.
