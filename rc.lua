4864d614e81861cb77c107f9fe9e4ca6bcb52f59
WooParadog <guohaochuan@gmail.com>
2025-10-27 21:28:48 +0800

    f

--- rc.lua
+++ rc.lua
@@ -46,7 +46,8 @@ awful.spawn.with_shell(
     'if (xrdb -query | grep -q "^awesome\\.started:\\s*true$"); then exit; fi;' ..
     'xrdb -merge <<< "awesome.started:true";' ..
     -- list each of your autostart commands, followed by ; inside single quotes, followed by ..
-    'dex --environment awesome --autostart' -- https://github.com/jceb/dex
+    'dex --environment awesome --autostart;' .. -- https://github.com/jceb/dex
+    'vicinae server;'
 )
 
 -- Variable definitions
